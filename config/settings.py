79f51f94d7aa36275e387e8f4c25373489b2a777
Liamhoulet10031988 <liam10031988@gmail.com>
2026-08-23 00:04:36 +0300

    Добавлены кеширование и сервисный слой каталога

--- config/settings.py
+++ config/settings.py
@@ -97,6 +97,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv(
+            "REDIS_URL",
+            "redis://127.0.0.1:6379/1",
+        ),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
